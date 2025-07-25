4af2b3b0f49a1b0d54afd9386d7c1fa21d692711
Фархутдинов Мидхат Р. <Studentbgau1@gmail.com>
2025-07-25 20:18:53 +0300

    Ещё пока на свайперах. Не понимаю как отрегулировать их количество нормально

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,49 @@
 // импортируем стили
-import './assets/scss/style.scss'
+import "./assets/scss/style.scss";
+
+// Ищет в дом дереве обёртку свайпера, куда нужно будет вставлять слайды
+const lists = document.querySelectorAll(".swiper-wrapper");
+const list1 = lists[0];
+const list2 = lists[1];
+const list3 = lists[2];
+const items1 = list1.children;
+const items2 = list2.children;
+const items3 = list3.children;
 
 // создаём объект, в который будем хранить свойства - ключ.
 const dataProperty = {
-  "Ремонтные услуги":1,
-  "Цена": 2,
-  "Срок": 3
-}
+  "Ремонтные услуги": 1,
+  Цена: 2,
+  Срок: 3,
+};
 
 // создаём переменные, которые будут ключами в объекте
-const data1 = ["Диагностика","Замена Дисплея","Замена полифонического динамика","Тестирование с выдачей технического заключения","Замена программного обеспечения"];
-const data2 = ["Бесплатно","1000 р","1000 р","1000 р","1000 р"];
-const data3 = ["30 мин","30-120 мин","30-120 мин","30-120 мин","30-120 мин"];
+const data1 = [
+  "Диагностика",
+  "Замена Дисплея",
+  "Замена полифонического динамика",
+  "Тестирование с выдачей технического заключения",
+  "Замена программного обеспечения",
+];
+const data2 = ["Бесплатно", "1000 р", "1000 р", "1000 р", "1000 р"];
+const data3 = [
+  "30 мин",
+  "30-120 мин",
+  "30-120 мин",
+  "30-120 мин",
+  "30-120 мин",
+];
 
 // переменная, которая будет хранить объекты
-const list123 = []
+const list123 = [];
 
 // прогоняем через функцию. Чтобы объект перестал быть ссылочным,
 // то мы ему зададим функцию структуредклон.
 for (let i = 0; i < 5; i++) {
   const try1 = structuredClone(dataProperty);
-  try1["Ремонтные услуги"] = data1[i]
-  try1['Цена'] = data2[i]
-  try1['Срок'] = data3[i]
+  try1["Ремонтные услуги"] = data1[i];
+  try1["Цена"] = data2[i];
+  try1["Срок"] = data3[i];
 
   list123.push(try1);
 }
@@ -40,11 +61,11 @@ const getSwiper = (swiper123) =>
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
-    }
+    },
   });
 
-  // объявляем свайпер функцию для 3 свайпера
-  const getSwiper2 = (swiper123) =>
+// объявляем свайпер функцию для 3 свайпера
+const getSwiper2 = (swiper123) =>
   new window.Swiper(swiper123, {
     // Optional parameters
     direction: "horizontal",
@@ -56,45 +77,35 @@ const getSwiper = (swiper123) =>
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
-    }
+    },
   });
 
-// Ищет в дом дереве обёртку свайпера, куда нужно будет вставлять слайды
-const lists = document.querySelectorAll(".swiper-wrapper");
-const list1 = lists[0];
-const list2 = lists[1];
-const list3 = lists[2];
-const items1 = list1.children;
-const items2 = list2.children
-const items3 = list3.children
-
-
 // теперь ищем образец, по которому будем делать слайды
-const swiperTemplate = document.querySelector("#slide-template").content
+const swiperTemplate = document.querySelector("#slide-template").content;
 const newSlideTemplates = swiperTemplate.querySelectorAll(".swiper-slide");
 const newSlideTemplates1 = newSlideTemplates[0];
 const newSlideTemplates2 = newSlideTemplates[1];
 const newSlideTemplates3 = newSlideTemplates[2];
 
 // создаём функцию для добавления слайдов
-const addSlide = function (template,list,logotype,index,array) {
+const addSlide = function (template, list, logotype, index, array) {
   const slide = template.cloneNode(true);
   if (logotype) {
-      const logo = slide.querySelector(".swiper__logoplace");
-      logo.style.backgroundImage = `url('${logotype}')`;
+    const logo = slide.querySelector(".swiper__logoplace");
+    logo.style.backgroundImage = `url('${logotype}')`;
   }
 
-   if (index && !array) {
-    const text = slide.querySelector(".swiper__text")
-    text.textContent = `Ремонт ${index}`
-    console.log('slava Iisusu!');
+  if (index && !array) {
+    const text = slide.querySelector(".swiper__text");
+    text.textContent = `Ремонт ${index}`;
+    console.log("slava Iisusu!");
   }
 
   if (array) {
     const topics = slide.querySelectorAll("span");
-    topics[0].textContent = array[index]["Ремонтные услуги"]
-    topics[1].textContent = array[index]['Цена']
-    topics[2].textContent = array[index]['Срок']
+    topics[0].textContent = array[index]["Ремонтные услуги"];
+    topics[1].textContent = array[index]["Цена"];
+    topics[2].textContent = array[index]["Срок"];
   }
   list.appendChild(slide);
 };
@@ -102,133 +113,186 @@ const addSlide = function (template,list,logotype,index,array) {
 // запускаем прорисовку слайдов
 const render = function () {
   for (let i = 1; i <= 8; i++) {
-    addSlide(newSlideTemplates1,list1,`/test/public/images/logo-${i}.png`);
+    addSlide(newSlideTemplates1, list1, `/test/public/images/logo-${i}.png`);
   }
 
   for (let i = 1; i <= 3; i++) {
-    addSlide(newSlideTemplates1,list1,`/test/public/images/logo-${i}.png`);
+    addSlide(newSlideTemplates1, list1, `/test/public/images/logo-${i}.png`);
   }
 
-    for ( let i = 1; i <= 8; i++) {
-    addSlide(newSlideTemplates2,list2, '', i);
+  for (let i = 1; i <= 8; i++) {
+    addSlide(newSlideTemplates2, list2, "", i);
   }
 
-      for ( let i = 0; i < 5; i++) {
-    addSlide(newSlideTemplates3,list3, '', i, list123);
+  for (let i = 0; i < 5; i++) {
+    addSlide(newSlideTemplates3, list3, "", i, list123);
   }
 };
 
 render();
 
-
-
+// Делаем интерактивность для свайперов.
 // в начале ищем кнопки Показать всё
 const btnHandlers = document.querySelectorAll(".swiper__handler");
 const btnHandler1 = btnHandlers[0];
 const btnHandler2 = btnHandlers[1];
 const btnHandler3 = btnHandlers[2];
 
-
 // создаём функцию, которая будет создавать прослушиватель события
-const inputFunction = function (btnHandler,items,list) {
+const inputFunction = function (btnHandler, items, list, count) {
+  if (window.innerWidth > 768) {
+    for (let i = count; i < items.length; i++) {
+      items[i].style.display = "none";
+    } } else {
+      if (window.innerWidth > 1024) {
+    for (let i = count + 2; i < items.length; i++) {
+      items[i].style.display = "none";
+    }
+    }
+  }
+  
+  window.addEventListener('resize', function () {
+  if (window.innerWidth > 768) {
+    for (let i = count; i < items.length; i++) {
+      items[i].style.display = "none";
+    }
+  }
+  })
+
   btnHandler.addEventListener("click", function () {
     const text = btnHandler.textContent;
     console.log(text);
     if (text === "Показать все") {
       for (let i = 0; i < items.length; i++) {
-        items[i].classList.add("swiper__show");
+        items[i].style.display = "flex";
       }
       btnHandler.textContent = "Скрыть";
       list.style.marginBottom = "45px";
       console.log(text);
     } else {
-      for (let i = 0; i < items.length; i++) {
-        items[i].classList.remove("swiper__show");
+      for (let i = count; i < items.length; i++) {
+        items[i].style.display = "none";
       }
       btnHandler.textContent = "Показать все";
       list.style.marginBottom = "24px";
     }
-  } );
-}
+  });
+};
 
+// теперь создаём прослушиватель события через функцию.
+inputFunction(btnHandler1, items1, list1, 6);
+inputFunction(btnHandler2, items2, list2, 3);
+inputFunction(btnHandler3, items3, list3, 3);
+
+// Делаем интерактивность для левого и 2 правых менюшек
 // Создаём ДОМ переменные, находим элементы в хтмл документе
-const containerDOM = document.querySelector('.main__container');
-const headerDOM = document.querySelector('.header');
-const asideDOM1 = document.querySelectorAll('.aside')[0];
+const containerDOM = document.querySelector(".main__container");
+const headerDOM = document.querySelector(".header");
+const asideDOM1 = document.querySelectorAll(".aside")[0];
 const aside = asideDOM1; // Когда ширина экрана более 1440px, то нужно чтобы при открытии правых меню, был заблюрено левое меню
-const asideDOM2 = document.querySelectorAll('.aside')[1];
-const asideDOM3 = document.querySelectorAll('.aside')[2];
+const asideDOM2 = document.querySelectorAll(".aside")[1];
+const asideDOM3 = document.querySelectorAll(".aside")[2];
 // const asideDOM3 = document.querySelectorall('.aside')[2];
 
 // ищем бургер
-const btnBurgerHandler = document.querySelector(".btn__burger")
+const btnBurgerHandler = document.querySelector(".btn__burger");
 // Ищем крестик
 const btnCrossHandler1 = document.querySelectorAll(".btn--cancellation")[0];
 const btnCrossHandler2 = document.querySelectorAll(".btn--cancellation")[1];
 const btnCrossHandler3 = document.querySelectorAll(".btn--cancellation")[2];
 // Ищем сообщения
-const btnFeedbackHandler1 = document.querySelectorAll('.btn--message')[0];
-const btnFeedbackHandler2 = document.querySelectorAll('.btn--message')[1];
+const btnFeedbackHandler1 = document.querySelectorAll(".btn--message")[0];
+const btnFeedbackHandler2 = document.querySelectorAll(".btn--message")[1];
 // Ищем звоночки
-const btnCallHandler1 = document.querySelectorAll('.btn--call')[0];
-const btnCallHandler2 = document.querySelectorAll('.btn--call')[1];
-
+const btnCallHandler1 = document.querySelectorAll(".btn--call")[0];
+const btnCallHandler2 = document.querySelectorAll(".btn--call")[1];
 
 // Функция. 1 параметр - кнопка, раскрывающая меню. 2 параметр - какое меню раскрыть. 3 параметр - кнопка закрывающая. 4,5 параметры - если нажать на другое меню, пока раскрыто 1, не должно закрываться 1 меню.
-const inputFunction2 = function (btnBurgerHandler,asideDOM1,btnCrossHandler1,asideDOM2,asideDOM3) {
-btnBurgerHandler.addEventListener('click', function() {
-  asideDOM1.style.display = "flex";
-  headerDOM.classList.add('blur');
-  containerDOM.classList.add('blur');
-  console.log(window.getComputedStyle(asideDOM1).display === "flex");
-  if (window.innerWidth > 1440) {
-  aside.classList.add('blur');
-  }
-})
-
-btnCrossHandler1.addEventListener('click', function() {
-  asideDOM1.style.display = "none";
-  headerDOM.classList.remove('blur');
-  containerDOM.classList.remove('blur');
-  console.log(window.getComputedStyle(asideDOM1).display === "flex");
+const inputFunction2 = function (
+  btnBurgerHandler,
+  asideDOM1,
+  btnCrossHandler1,
+  asideDOM2,
+  asideDOM3,
+) {
+  btnBurgerHandler.addEventListener("click", function () {
+    asideDOM1.style.display = "flex";
+    headerDOM.classList.add("blur");
+    containerDOM.classList.add("blur");
+    console.log(window.getComputedStyle(asideDOM1).display === "flex");
     if (window.innerWidth > 1440) {
-  aside.classList.remove('blur');
-  }
-})
+      aside.classList.add("blur");
+    }
+  });
 
-window.addEventListener('click', function (e) {
+  btnCrossHandler1.addEventListener("click", function () {
+    asideDOM1.style.display = "none";
+    headerDOM.classList.remove("blur");
+    containerDOM.classList.remove("blur");
+    console.log(window.getComputedStyle(asideDOM1).display === "flex");
+    if (window.innerWidth > 1440) {
+      aside.classList.remove("blur");
+    }
+  });
 
-if (asideDOM1.contains(e.target) === false && window.getComputedStyle(asideDOM1).display === "flex" && asideDOM2.contains(e.target) === false && asideDOM3.contains(e.target) === false) {
-  console.log('popitka');
-  asideDOM1.style.display = "none";
-  headerDOM.classList.remove('blur');
-  containerDOM.classList.remove('blur');
-  if (window.innerWidth > 1440) {
-  aside.classList.remove('blur');
-  }
-  }
-}, {capture:true})
+  window.addEventListener(
+    "click",
+    function (e) {
+      if (
+        asideDOM1.contains(e.target) === false &&
+        window.getComputedStyle(asideDOM1).display === "flex" &&
+        asideDOM2.contains(e.target) === false &&
+        asideDOM3.contains(e.target) === false
+      ) {
+        console.log("popitka");
+        asideDOM1.style.display = "none";
+        headerDOM.classList.remove("blur");
+        containerDOM.classList.remove("blur");
+        if (window.innerWidth > 1440) {
+          aside.classList.remove("blur");
+        }
+      }
+    },
+    { capture: true },
+  );
+};
 
-}
-
-inputFunction2(btnBurgerHandler,asideDOM1,btnCrossHandler1,asideDOM2,asideDOM3);
-inputFunction2(btnFeedbackHandler1,asideDOM2,btnCrossHandler2,asideDOM1,asideDOM3);
-inputFunction2(btnFeedbackHandler2,asideDOM2,btnCrossHandler2,asideDOM1,asideDOM3);
-inputFunction2(btnCallHandler1,asideDOM3,btnCrossHandler3,asideDOM1,asideDOM2);
-inputFunction2(btnCallHandler2,asideDOM3,btnCrossHandler3,asideDOM1,asideDOM2);
-
-
-
-
-
-// теперь создаём прослушиватель события через функцию.
-inputFunction(btnHandler1,items1,list1)
-inputFunction(btnHandler2,items2,list2)
-inputFunction(btnHandler3,items3,list3)
-
-
-
-
+// Активируем взаимодействия со всеми 3 менюшками.
+inputFunction2(
+  btnBurgerHandler,
+  asideDOM1,
+  btnCrossHandler1,
+  asideDOM2,
+  asideDOM3,
+);
+inputFunction2(
+  btnFeedbackHandler1,
+  asideDOM2,
+  btnCrossHandler2,
+  asideDOM1,
+  asideDOM3,
+);
+inputFunction2(
+  btnFeedbackHandler2,
+  asideDOM2,
+  btnCrossHandler2,
+  asideDOM1,
+  asideDOM3,
+);
+inputFunction2(
+  btnCallHandler1,
+  asideDOM3,
+  btnCrossHandler3,
+  asideDOM1,
+  asideDOM2,
+);
+inputFunction2(
+  btnCallHandler2,
+  asideDOM3,
+  btnCrossHandler3,
+  asideDOM1,
+  asideDOM2,
+);
 
 // Что делать, чтобы слайды при резайзе прорисовывались?
 let swiper1 = null;
@@ -241,8 +305,6 @@ window.addEventListener("resize", () => {
     swiper1 = getSwiper(".swiper1");
   } else {
     if (!swiper1) return;
-    console.log(swiper1.destroy, 333);
-    console.log(swiper1,444);
     swiper1.destroy();
     swiper1 = null;
   }
